f2ea610af37970be66f481d0387885b2a67f87b7
Salva Pérez <salva.perez46@gmail.com>
2020-12-09 21:10:37 +0100

    Improved documentation
    - Readme updated
    - Added pydoc to data service interfaces

--- transpydata/config/IResourceAware.py
+++ transpydata/config/IResourceAware.py
@@ -4,9 +4,13 @@ from abc import ABCMeta, abstractmethod
 class IResourceAware(metaclass=ABCMeta):
     @abstractmethod
     def initialize(self):
+        """ Method to initialize resources needed by the class.
+        """
         pass
 
     @abstractmethod
     def dispose(self):
+        """ Method to dispose resources acquired by the class.
+        """
         pass
 
